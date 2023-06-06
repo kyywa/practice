f64f543fd5a6598838f14aeea461b0b98f620281
Kyywa <jbence357@gmail.com>
2023-06-06 05:02:11 +0200

    practice

---
+++ AutoPiacWPF/AutoPiacWPF/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutoPiacWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Login(object sender,StartupEventArgs e)
+        {
+            Login login=new Login();
+            Application.Current.MainWindow = login;
+            MainWindow window = new MainWindow();
+            login.ShowDialog();
+            window.Show();
+        }
+    }
+}
